46bdc2f639b2d32031c56f90be27536dd53424ee
Jesse M. Holmes <jholmes@rxnt.com>
2019-01-28 16:17:20 -0500

    01 Hello React: Rename files and components to reflect React conventions

--- 01 Hello React/src/hello.tsx
+++ 01 Hello React/src/hello.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export const HelloComponent: React.FunctionComponent<{}> = () => {
+export const Hello: React.FunctionComponent<{}> = () => {
   return (
     <h2>Hello, React!</h2>
   );
--- 01 Hello React/src/index.tsx
+++ 01 Hello React/src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { HelloComponent } from './hello';
+import { Hello } from './hello';
 
     ReactDOM.render(
-        <HelloComponent/>,
+        <Hello />,
         document.getElementById('root')
     );
